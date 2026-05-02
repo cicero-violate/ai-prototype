4f3c7b33a366f3c4f4b458c6cef7273dd8054610
Cicero Bugash <cicero.bugash@gmail.com>
2026-05-01 22:43:11 -0400

    auto

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 git add .
 git commit -m "uploading to chatgpt projects"
 git push origin main
+git rev-parse HEAD
 
 python3 /workspace/ai_sandbox/canon-mini-agent/prototype/cdp-file-upload/upload_via_cdp.py \
   --build-tar \
