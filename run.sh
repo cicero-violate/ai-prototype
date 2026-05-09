e2e7e4c7d7bc8a0f1ac9a182c2c840fa134d87bb
Cicero Bugash <cicero.bugash@gmail.com>
2026-05-09 15:48:49 -0400

    auto

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # # Minimal: loop + certification against a running worker
-# AI_WORKER_PORT=9091 \
-# PROJECT_DIR=. \
-# CANON_OPENAI_BASE_URL=http://127.0.0.1:8081/v1 \
-# ./target/release/agent
+AI_WORKER_PORT=9091 \
+PROJECT_DIR=. \
+CANON_OPENAI_BASE_URL=http://127.0.0.1:8081/v1 \
+./target/release/agent
 
 # With supervisor auto-reload for a fresh worker each cycle
 AI_WORKER_PORT=9091 \
