0b7d4a9929a16413d21bdae2eda4f84c8fe53760
Cicero Bugash <cicero.bugash@gmail.com>
2026-05-09 15:49:10 -0400

    auto

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ CANON_OPENAI_BASE_URL=http://127.0.0.1:8081/v1 \
 ./target/release/agent
 
 # With supervisor auto-reload for a fresh worker each cycle
-AI_WORKER_PORT=9091 \
-SUPERVISOR_PORT=9090 \
-PROJECT_DIR=. \
-CANON_OPENAI_BASE_URL=http://127.0.0.1:8081/v1 \
-./target/release/agent
+# AI_WORKER_PORT=9091 \
+# SUPERVISOR_PORT=9090 \
+# PROJECT_DIR=. \
+# CANON_OPENAI_BASE_URL=http://127.0.0.1:8081/v1 \
+# ./target/release/agent
