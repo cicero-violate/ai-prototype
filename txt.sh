030b02e99a355bb54e604fdc1ba125201c1c1458
Cicero Bugash <cicero.bugash@gmail.com>
2026-05-13 14:57:50 -0400

    save

--- txt.sh
+++
@@ -1,9 +0,0 @@
-# Option A — build release
-cargo build --release
-
-# Option B — run the debug build
-AI_WORKER_BIN=./target/debug/worker \
-AI_TLOG_DIR=tlog \
-AI_MCP_WORKER_URL=http://127.0.0.1:38469/mcp_worker \
-SUPERVISOR_PORT=9090 \
-./target/debug/supervisor
